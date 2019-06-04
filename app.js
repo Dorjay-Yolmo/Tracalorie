4b0ef32ca3fdb2eff2bf31860eb0020e1f892af5
Dorjay-Yolmo <50335936+Dorjay-Yolmo@users.noreply.github.com>
2019-06-05 03:20:40 +0530

    Edit mechanism being Implemented

--- app.js
+++ app.js
@@ -47,6 +47,21 @@ const ItemCtrl = (function () {
       return newItem;
     },
 
+    getItemById: function (id) {
+      let found = null;
+      // Loop through items
+      data.items.forEach(function (item) {
+        if (item.id === id) {
+          found = item;
+        }
+      });
+      return found;
+    },
+
+    setCurrentItem: function (item) {
+      data.currentItem = item;
+    },
+
     getTotalCalories: function () {
       let total = 0;
 
@@ -75,6 +90,9 @@ const UICtrl = (function () {
   const UISelectors = {
     itemList: '#item-list',
     addBtn: '.add-btn',
+    updateBtn: '.update-btn',
+    deleteBtn: '.delete-btn',
+    backBtn: '.back-btn',
     itemNameInput: '#item-name',
     itemCaloriesInput: '#item-calories',
     totalCalories: '.total-calories'
@@ -133,6 +151,14 @@ const UICtrl = (function () {
       document.querySelector(UISelectors.totalCalories).textContent = totalCalories;
     },
 
+    clearEditState: function () {
+      UICtrl.clearInput();
+      document.querySelector(UISelectors.updateBtn).style.display = 'none';
+      document.querySelector(UISelectors.deleteBtn).style.display = 'none';
+      document.querySelector(UISelectors.backBtn).style.display = 'none';
+      document.querySelector(UISelectors.addBtn).style.display = 'inline';
+    },
+
     getSelectors: function () {
       return UISelectors;
     }
@@ -150,6 +176,9 @@ const App = (function (ItemCtrl, UICtrl) {
 
     // Add item event
     document.querySelector(UISelectors.addBtn).addEventListener('click', itemAddSubmit);
+
+    // Edit item Event
+    document.querySelector(UISelectors.itemList).addEventListener('click', itemUpdateSubmit);
   }
 
   // Add item Submit
@@ -177,9 +206,32 @@ const App = (function (ItemCtrl, UICtrl) {
     e.preventDefault();
   }
 
+  // Update item Submit
+  const itemUpdateSubmit = function (e) {
+    if (e.target.classList.contains('edit-item')) {
+      // Get List item id (item-0, item-1)
+      const listId = e.target.parentNode.parentNode.id;
+
+      // Break into an Array
+      const listIdArr = listId.split('-');
+
+      // Get the Actual ID
+      const id = parseInt(listIdArr[1]);
+
+      // Get Item to Edit
+      const itemToEdit = ItemCtrl.getItemById(id);
+
+      ItemCtrl.setCurrentItem(itemToEdit);
+    }
+    e.preventDefault();
+  }
+
   // Public methods
   return {
     init: function () {
+      // Clear Edit Fields / Set Initial state
+      UICtrl.clearEditState();
+
       // Fetch items from data structure
       const items = ItemCtrl.getItems();
 
